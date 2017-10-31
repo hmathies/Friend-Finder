9d217c45b7870027cdf4b516178c80091e862e82
Heather <hlmathies@gmail.com>
2017-10-31 14:24:10 -0400

    Add basic html tmeplate

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,10 @@
+/*===========================================================================
+Requirements for this page:
+* A GET Route to `/survey` which should display the survey page.
+* A default, catch-all route that leads to `home.html` which displays the home page.
+*/
+// ===============================================================================
+// DEPENDENCIES
+//The path package gets the correct file path for our html
+// ===============================================================================
+var path = require("path");
