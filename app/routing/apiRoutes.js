877251144505602e31f7d08a977831d2fd78b990
Heather <hlmathies@gmail.com>
2017-11-02 16:14:10 -0400

    Create newfriendsData variable to hold new friend data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -42,16 +42,21 @@ module.exports = function(app) {
 //take in data from survey, This route will also be used to handle the compatibility logic.
 //for each or for in loop over the friends data
 //do the .push last--look up absolute value in js
+  var newfriendsData = req.body;
+  console.log(newfriendsData);
 
-    var total = 0
-    friendsData.forEach(function (friendsData) {
-      total += friendsData
-    });
-    console.log(total / friendsData.length)
+  friendsData.push(newfriendsData);
+  res.json(newfriendsData);
+
+//the below loop isn't wokring and the error is saying that it can't convert object to a primitive value
+    // var total = 0
+    // for (i in friendsData){
+    //   total += friendsData;
+    // };
+    // console.log(total / friendsData.length);
 
 
-    friendsData.push(req.body);
-console.log(friendsData);
+
     //send back the matched friend info /make sure the field i send back to the request has a name and a photo
   });
 
