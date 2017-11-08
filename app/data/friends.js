7fb9201d3066a9363d63d90a9d7653cb66291d88
Heather <hlmathies@gmail.com>
2017-11-08 15:38:26 -0500

    Add style to index and survey html

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,9 +6,14 @@
 
 var friendsData = [
   {
-    "name": "Heather",
+    "name": "Pamela",
     "photo": "https://images.pexels.com/photos/164262/pexels-photo-164262.jpeg?h=350&auto=compress&cs=tinysrgb",
     "scores": [1,5,3,2,1,5,4,4,2,2]
+  },
+  {
+    "name": "Julie",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxs_Xu2trmZ6Gm14Tj_AlYKTJnJhY0bfBOCj6ymEcr9NzLOWHDPg",
+    "scores": [1,2,2,2,1,5,3,4,2,2]
   }
 ];
 
